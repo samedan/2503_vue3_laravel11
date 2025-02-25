87e4d69ac2a9c9cf3f9e3466900df14bf03b4219
John Doe <johndoe@example.com>
2025-02-25 16:26:06 +0100

    Working with props

--- vue-composition-api/src/App.vue
+++ vue-composition-api/src/App.vue
@@ -1,65 +1,26 @@
 <script>
-import { ref, reactive, toRef, toRefs } from "vue";
+import CartItem from "./components/CartItem.vue";
+import { ref } from "vue";
 
 export default {
+    components: { CartItem },
     setup() {
-        const message = ref("Hello");
-        const quantity = ref(1);
-
-        const item = reactive({
-            name: "Product 1",
-            price: 10,
-        });
-
-        const increment = () => quantity.value++;
-        const decrement = () => quantity.value--;
-
-        const swapProduct = () => {
-            item.name = "Product A";
-            item.price = 30;
-        };
-
-        // const nameRef = toRef(item, "name");
-
-        // console.log("nameRef: ", nameRef.value);
-
-        // item.name = "New Product";
-
-        // console.log("nameRef: ", nameRef.value);
-
-        // const itemRefs = toRefs(item);
-        const { name, price } = toRefs(item);
-
-        // console.log("name", itemRefs.name.value);
-        // console.log("price", itemRefs.price.value);
-
-        // item.name = "New product";
-        // item.price = 50;
-
-        // console.log("name", itemRefs.name.value);
-        // console.log("price", itemRefs.price.value);
-
-        return {
-            message,
-            quantity,
-            increment,
-            decrement,
-            // item,
-            name,
-            price,
-            swapProduct,
-        };
+        const items = ref([
+            {
+                id: 1,
+                name: "product X",
+                price: 20,
+                quantity: 1,
+            },
+        ]);
+        return { items };
     },
 };
 </script>
 
 <template>
     <div>
-        <h1>{{ name }} : {{ price }}</h1>
-        <button @click="swapProduct">Swap product</button>
-        <h2>{{ quantity }}</h2>
-        <button @click="increment">+</button>
-        <button @click="decrement">-</button>
+        <CartItem v-for="item in items" :cart-item="item" :key="item.id" />
     </div>
 </template>
 
---
+++ vue-composition-api/src/components/CartItem.vue
@@ -0,0 +1,46 @@
+<script>
+import { computed, reactive, toRefs } from "vue";
+
+export default {
+    props: {
+        cartItem: {
+            type: Object,
+            required: true,
+        },
+    },
+
+    setup(props) {
+        const item = reactive(props.cartItem);
+
+        const increment = () => item.quantity++;
+        const decrement = () => item.quantity--;
+
+        const total = computed(() => item.price * item.quantity);
+
+        const { name, price, quantity } = toRefs(item);
+
+        return {
+            quantity,
+            increment,
+            decrement,
+            // item,
+            name,
+            price,
+            total,
+        };
+    },
+};
+</script>
+
+<template>
+    <div>
+        <h1>{{ name }} : {{ price }}: {{ quantity }}</h1>
+
+        <button @click="increment">+</button>
+        <button @click="decrement">-</button>
+
+        <h3>Total: {{ total }}</h3>
+    </div>
+</template>
+
+<style scoped></style>
