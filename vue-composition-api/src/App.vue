2615e649443aba7698a2f4dfef446e0dd431cda1
John Doe <johndoe@example.com>
2025-02-25 14:33:38 +0100

    Vue Composition API

---
+++ vue-composition-api/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    setup() {
+        const message = "Hello";
+        return {
+            message,
+        };
+    },
+};
+</script>
+
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<style scoped></style>
